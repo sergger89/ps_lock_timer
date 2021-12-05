c97192bff79392dae8a9544971ba7c3f4d7bcb69
unknown <serg.ger89@gmail.com>
2021-12-05 21:35:50 +0300

    clear leftresults

--- locktimer.ps1
+++ locktimer.ps1
@@ -4,8 +4,9 @@ $array=1..$time
 foreach ($item in $array) {
     sleep 60
     $left=$time-$item
+    clear
     Write-Host $left minutes left 
 }
 Write-Host time is up!
 sleep 3
-rundll32.exe user32.dll,LockWorkStation
+#rundll32.exe user32.dll,LockWorkStation
